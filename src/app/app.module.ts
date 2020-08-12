fd9596fa883e974187b1750d82471808c96826ba
Jim Drury <jim@jimdrury.co.uk>
2020-08-12 10:20:21 +0100

    feat(footer): adding footer to each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HeaderModule} from './common/header/header.module';
 import {ModalPortalModule} from './common/modal-portal/modal-portal.module';
+import {FooterModule} from './common/footer/footer.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
-    HeaderModule,
-    ModalPortalModule,
-  ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'serverApp'}),
+        AppRoutingModule,
+        HeaderModule,
+        ModalPortalModule,
+        FooterModule,
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
